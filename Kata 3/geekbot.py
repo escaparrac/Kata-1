59c6404c1d4566fb138c18d5ca903881edca9026
Escaparrac <juakin.rrpp@gmail.com>
2020-05-16 13:14:59 +0200

    aa

--- Kata 3/geekbot.py
+++ Kata 3/geekbot.py
@@ -1,10 +1,21 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 
 def main():
-    updater = Update(token=open("./bot_token").read(), use_context=True)
+    # Instanciamos el updator
+    updater = Updater(token=open("./bot_token").read(), use_context=True)
+
+    # Añadiendo un manejador al comando /start
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    # Empezamos a pedir notificaciones a Telegram
     updater.start_polling()
+    
+    # Capturamos señales de parada
     updater.idle()
+
+    def start(update, context):
+    pass
 
 
 main()
